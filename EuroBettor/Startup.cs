c703256e4d2acaae03f2dbfc73b07b2d69b6c2c6
Christofer Ohlsson <christofer.ohlsson@gmail.com>
2016-06-05 22:18:27 +0200

    Add project files.

---
+++ EuroBettor/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EuroBettor.Startup))]
+namespace EuroBettor
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
